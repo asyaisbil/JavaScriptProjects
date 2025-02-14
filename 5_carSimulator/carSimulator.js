b954d76ef9b750d494faed9702a0235c05de3987
Asya <asyaisbill@gmail.com>
2025-02-14 23:05:47 +0300

    Update carSimulator.js

--- 5_carSimulator/carSimulator.js
+++ 5_carSimulator/carSimulator.js
@@ -1,3 +1,6 @@
+
+// Creating a Car
+
 function getCarObject(id, model, yakit, vites, yas, fiyat, kilometre) {
     newCarObject = {
         id: id,
@@ -12,3 +15,32 @@ function getCarObject(id, model, yakit, vites, yas, fiyat, kilometre) {
 }
 
 //console.log(getCarObject(121, 'VW Passat', 'Benzin', 'Otomatik', 11, 800000, 153000));
+
+
+
+// Reviews
+
+function getBelowTheAverage(reviews) {
+    let sumPoint = 0;
+    for (i = 0; i < reviews.length; i++) {
+        sumPoint = (sumPoint + reviews[i].point);
+    }
+    const avgPoint = sumPoint / reviews.length;
+    //console.log(avgPoint);  
+    const belowAverage = [];
+    for (i = 0; i < reviews.length; i++) {
+        if (avgPoint > reviews[i].point) {
+            belowAverage.push(reviews[i].name);
+            //console.log(reviews[i].name, reviews[i].point);
+            //console.log(belowAverage);
+        }
+    }
+    return belowAverage;
+}
+
+//console.log(getBelowTheAverage(reviews));
+
+
+
+// Updating Car Infos
+
