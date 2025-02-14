a0423cc7f85db13931023f9dd6fdbb097ef15608
Asya <asyaisbill@gmail.com>
2025-02-14 22:16:45 +0300

    Update carSimulator.js

--- 5_carSimulator/carSimulator.js
+++ 5_carSimulator/carSimulator.js
@@ -1,1 +1,14 @@
+function getCarObject(id, model, yakit, vites, yas, fiyat, kilometre) {
+    newCarObject = {
+        id: id,
+        model: model,
+        yakit: yakit, 
+        vites: vites, 
+        uretimYili: new Date().getFullYear() - yas, 
+        fiyat: fiyat, 
+        kilometre: kilometre
+    }
+    return newCarObject;
+}
 
+//console.log(getCarObject(121, 'VW Passat', 'Benzin', 'Otomatik', 11, 800000, 153000));
